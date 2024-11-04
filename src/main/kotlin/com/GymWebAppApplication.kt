96ab94855839edb5796cc2a01bc804956ecbf81d
Ariel Citrangulo <arielcitrangulo355@gmail.com>
2024-11-04 19:29:59 -0300

    inicio de tudo

---
+++ src/main/kotlin/com/GymWebAppApplication.kt
@@ -0,0 +1,11 @@
+package com
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GymWebAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<GymWebAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/gymwebapp/GymWebApp/GymWebAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gymwebapp.GymWebApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GymWebAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
