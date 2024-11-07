14c4f82fa8add1e0a6c47a2bc57e1d437dfa5078
Ariel Citrangulo <arielcitrangulo355@gmail.com>
2024-11-06 21:27:16 -0300

    Finalizado a lógica de deleção do banco e tabelas web

--- src/main/kotlin/com/gymwebapp/controller/ExercicioController.kt
+++ src/main/kotlin/com/gymwebapp/controller/ExercicioController.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 
 
@@ -20,20 +21,61 @@ class ExercicioController {
 
 
         
-        val treino = Treino()
+        var treino = Treino()
 
         model.addAttribute("treino", treino)
 
         return "CadastroTreino"
     }
 
-    @PostMapping("/cadastrar")
+    @PostMapping("/cadastrar") // Salva os treinos no Banco
     fun cadastrarExercicio(treino: Treino): String{
 
         println(treino)
         treinoRepository.save(treino)
 
+        return "redirect:/TreinoCadastrado"
+
+    }
+
+    @GetMapping("/TreinoCadastrado") // Select os treinos do Banco
+    fun listaTreino(model: Model): String{
+
+        val treinos = treinoRepository.findAll()
+
+        model.addAttribute("treinos", treinos)
+
         return "Inicial"
 
     }
+
+    @GetMapping("/treino/edicao/{id}")
+    fun tablelaEditarExercicio(@PathVariable("id") id:Long, model: Model) : String{
+
+        var treino = treinoRepository.findById(id).orElse(null)
+
+        model.addAttribute("treino", treino)
+
+        return "EdicaoTreino"
+    }
+
+    @PostMapping("/editar/{id}")
+    fun editarTreino(@PathVariable("id") id: Long, treino: Treino): String {
+
+        treino.id = id
+
+        treinoRepository.save(treino)
+
+        return "redirect:/TreinoCadastrado"
+    }
+
+    @GetMapping("/excluir/{id}")
+    fun excluirExercicio(@PathVariable("id") id: Long) : String{
+
+        treinoRepository.deleteById(id)
+
+        return "redirect:/TreinoCadastrado"
+    }
+
+
 }
--- src/main/kotlin/com/gymwebapp/model/Treino.kt
+++ src/main/kotlin/com/gymwebapp/model/Treino.kt
@@ -11,7 +11,7 @@ data class Treino (
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    val id: Long? = null,
+    var id: Long? = null,
 
     val exercicio: String = "",
     val series: String = "",
